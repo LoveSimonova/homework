f51941b272c54f058c518ed28eee87289ca7e148
«SimLove» <simonolyuba@yandex.ru>
2023-05-25 23:04:51 +0300

    task2...

---
+++ task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber>secondNumber)
+{
+    Console.WriteLine("max = "+firstNumber);
+}
+else
+    Console.WriteLine("max = "+secondNumber);
