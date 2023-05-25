be8b45ce9daea69f20f37ffce8770a837a3d6075
Love_Sim <simonolyuba@yandex.ru>
2023-05-25 22:24:14 +0300

    Add task4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber>secondNumber)
+    if(firstNumber>thirdNumber)
+        Console.WriteLine("max = "+firstNumber);
+    else
+        Console.WriteLine("max = "+thirdNumber);
+else
+    if(secondNumber>thirdNumber)
+        Console.WriteLine("max = "+secondNumber);
+    else
+        Console.WriteLine("max = "+thirdNumber);
