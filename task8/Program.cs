95c64e3ca75789da6733685e7b7d522bb9dd14e4
Love_Sim <simonolyuba@yandex.ru>
2023-05-25 22:36:16 +0300

    Add task8

---
+++ task8/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+while (number2<=number)
+{
+    Console.Write(number2+" ");
+    number2+=2;
+}
